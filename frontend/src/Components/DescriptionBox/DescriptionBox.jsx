56c2c48c6d0d0f9cb5c63ea6a9fdad6a90306f97
Vonglory176 <SkylerGConley@gmail.com>
2024-03-29 18:48:12 -0700

    Basic component to display product details

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda voluptate placeat soluta porro earum esse officiis iste optio fuga labore consequuntur unde eos architecto repellat, perspiciatis eius illum nemo quibusdam.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore earum asperiores libero voluptates pariatur eveniet accusamus. Perferendis rem iure incidunt.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
