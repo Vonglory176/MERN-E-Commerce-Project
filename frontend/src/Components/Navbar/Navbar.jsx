e2a9ba7859a67de3413ecb997e80d133c4f45848
Vonglory176 <SkylerGConley@gmail.com>
2024-03-29 18:49:39 -0700

    Standard navigation bar with minor interactibility

---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react' //Typing "rafc" will create a 'r.eact a.rrow f.unctional c.omponent'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    const [menu, setMenu] = useState("shop")
+    const categories = ["shop", "men", "women", "kids"]
+
+  return (
+    <div className='navbar'>
+        <div className="nav-logo">
+            <img src={logo} alt="" />
+            <p>SHOPPER</p>
+        </div>
+
+        <ul className="nav-menu">
+            {categories.map((category, i) => (
+                <li 
+                key={i}
+                onClick={() => setMenu(category)}>
+                    <Link                     
+                    to={category === "shop"? "/" : `/${category}`} 
+                    title={category === "shop" ? "Browse all products" : `Browse products for ${category}`}
+                    style={{textDecoration: "none"}}>
+                        {category.toUpperCase()}{menu === category && <hr/>}
+                    </Link>
+                </li>
+            ))}
+
+            {/* <li onClick={() => setMenu("shop")}><Link to="/">Shop{menu === "shop" && <hr/>}</Link></li>
+            <li onClick={() => setMenu("men")}><Link to="/men">Men{menu === "men" && <hr/>}</Link></li>
+            <li onClick={() => setMenu("women")}><Link to="/women">Women{menu === "women" && <hr/>}</Link></li>
+            <li onClick={() => setMenu("kids")}><Link to="/kids">Kids{menu === "kids" && <hr/>}</Link></li> */}
+        </ul>
+
+        <div className="nav-login-cart">
+            <Link to="/login"><button>Login</button></Link>
+            <Link to="/cart"><img src={cart_icon} alt="" /></Link>
+            <div className="nav-cart-count">0</div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
