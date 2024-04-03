cee8ed2e438c0ff6307af828c34222cc90d05e9c
Vonglory176 <SkylerGConley@gmail.com>
2024-04-03 15:29:21 -0700

    Recently added products are now fetched from backend

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import new_collection from '../Assets/new_collections'
+import React, { useState } from 'react'
 import Item from '../Item/Item'
+import { useEffect } from 'react'
+// import new_collection from '../Assets/new_collections'
 
 const NewCollections = () => {
+
+  const [new_collection, setNew_collection] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/newcollections')
+    .then((response) => response.json())
+    .then((data) => setNew_collection(data))
+  }, [])
+
   return (
     <div className='new-collections'>
       <h1>NEW COLLECTIONS</h1>
