d107edc28b20c8faa6ef75b5e52c6b3ff70db608
Vonglory176 <SkylerGConley@gmail.com>
2024-04-03 15:29:48 -0700

    Products and collections are now loaded from backend on first mount

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,5 +1,5 @@
 import React, {createContext, useEffect, useState} from "react"
-import all_product from '../Components/Assets/all_product.js'
+// import all_product from '../Components/Assets/all_product.js'
 
 
 // Creating a new Context object for the shop. This will be used to pass data deeply throughout the component tree without having to explicitly pass props down at every level.
@@ -7,7 +7,8 @@ export const ShopContext = createContext(null);
 
 const getDefaultCart = () => { //On initialization, create a list of all products with quantity 0
     let cart = {}
-    for (let i = 0; i < all_product.length+1; i++) {
+    // for (let i = 0; i < all_product.length+1; i++) {
+    for (let i = 0; i < 300+1; i++) {
         cart[i] = 0
     }
     return cart
@@ -15,7 +16,15 @@ const getDefaultCart = () => { //On initialization, create a list of all product
 
 // This component will wrap part of our app where we want the shop's data to be accessible.
 const ShopContextProvider = (props) => {
+
+    const [all_product, setAll_Product] = useState([])
     const [cartItems, setCartItems] = useState(getDefaultCart())
+
+    useEffect(() => {
+        fetch('http://localhost:4000/allproducts')
+        .then((response) => response.json())
+        .then((data) => setAll_Product(data.products))
+    }, [])
 
     const addToCart = (itemId) => {
         setCartItems(prev => ({...prev, [itemId]: prev[itemId] + 1})) // Adding to the selected item        
